c582ee3c9f89461c6f89ba0d0d94744d05a681be
Kuro Reichi. <95631442+KuroReichi@users.noreply.github.com>
2026-09-15 15:05:26 +0700

    feat: add no render spawner client entrypoint

---
+++ tools/no-render-mob-spawner/src/client/java/com/kuroreichi/norenderspawner/NoRenderMobSpawnerClient.java
@@ -0,0 +1,11 @@
+package com.kuroreichi.norenderspawner;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class NoRenderMobSpawnerClient implements ClientModInitializer {
+    public static final NoRenderMobSpawnerConfig CONFIG = NoRenderMobSpawnerConfig.load();
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
