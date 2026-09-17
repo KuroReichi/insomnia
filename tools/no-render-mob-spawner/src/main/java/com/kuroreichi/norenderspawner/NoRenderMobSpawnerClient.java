66ca4d5da840cfd1472ceb88dca4852cfdeac6a5
Kuro Reichi. <95631442+KuroReichi@users.noreply.github.com>
2026-09-17 18:41:09 +0700

    fix: initialize model optimization in main source

--- tools/no-render-mob-spawner/src/main/java/com/kuroreichi/norenderspawner/NoRenderMobSpawnerClient.java
+++ tools/no-render-mob-spawner/src/main/java/com/kuroreichi/norenderspawner/NoRenderMobSpawnerClient.java
@@ -1,11 +1,13 @@
 package com.kuroreichi.norenderspawner;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
 
 public final class NoRenderMobSpawnerClient implements ClientModInitializer {
     public static final NoRenderMobSpawnerConfig CONFIG = NoRenderMobSpawnerConfig.load();
 
     @Override
     public void onInitializeClient() {
+        ModelLoadingPlugin.register(new NoRenderMobSpawnerModelPlugin());
     }
 }
